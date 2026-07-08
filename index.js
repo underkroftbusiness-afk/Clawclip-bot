fc7775eb858729e6cd19a4767e4dc151989f54b1
underkroftbusiness-afk <underkroftbusiness@gmail.com>
2026-07-08 21:33:50 +0200

    Add basic Discord bot functionality
    “Eerste versie van Clawclip‑bot met eenvoudige ping‑command.

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot is online as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
